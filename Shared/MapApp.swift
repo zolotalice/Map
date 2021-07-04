d70e0b79df64f3414939965857e4a6e2fa736814
ZolotAlice <zolotalice@gmail.com>
2021-07-04 16:05:46 +0300

    Initial Commit

---
+++ Shared/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Shared
+//
+//  Created by Alice Zolotareva on 04.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
